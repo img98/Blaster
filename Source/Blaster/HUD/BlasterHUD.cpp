aacc1484baac401b2bbd8280a78a328e9577ab65
img98 <nichy98@naver.com>
2022-12-23 21:50:39 +0900

    feat : 플레이어컨트롤러, HUD cpp파일 초안 생성

---
+++ Source/Blaster/HUD/BlasterHUD.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlasterHUD.h"
+
+void ABlasterHUD::DrawHUD()
+{
+	Super::DrawHUD();
+
+	//TODO: 무기에 맞는 크로스헤어 그리기
+}
